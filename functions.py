1359353fe2f6f63eb4db4c8e014f96201771354c
catalyst-1 <84675268+catalyst-1@users.noreply.github.com>
2022-03-23 15:15:43 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,8 +1,4 @@
-def combine(array , pointer):
-#pointer points where to get value
-        if pointer == 0:
-                return array
-        return combine(array,pointer-1) + array[pointer-1]
+
 def magic(str):
         for ai in str:
                 for bi in str:
